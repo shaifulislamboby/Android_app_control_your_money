e61201fddc33d858ed7b9b35d1961c035e0472ba
islam <islam@gmail.com>
2020-05-16 11:19:48 +0200

    adding scroolview to home page

--- app/src/main/java/com/example/money2/MainActivity.java
+++ app/src/main/java/com/example/money2/MainActivity.java
@@ -34,8 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // R.menu.mymenu is a reference to an xml file named mymenu.xml which should be inside your res/menu directory.
-        // If you don't have res/menu, just create a directory named "menu" inside res
+
         getMenuInflater().inflate(R.menu.menus, menu);
         return super.onCreateOptionsMenu(menu);
     }
